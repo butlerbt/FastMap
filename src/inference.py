1a45101a71b5efeab4f9d7a21006560b89a4c5ad
Brent <butlerbt.mg@gmail.com>
2020-02-10 22:59:22 -0800

    creat folium map and use phantomjs to screen shot

--- src/inference.py
+++ src/inference.py
@@ -27,25 +27,38 @@ def prep_input(input_img):
     return t_img
 
 
-def make_inference(model, t_img):
+def inference_mask(model, t_img):
     outputs = model.predict(t_img)
-    inference_mask = image2np(outputs[2].sigmoid())
+    inference_mask = image2np(outputs[2])
     inference_mask = (inference_mask*255).astype('uint8')
     return inference_mask
 
+# def visualize_inference(inference_mask, input_img):
+    
+#     fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,5))
+#     ax1.imshow(io.imread(input_img))
+#     ax2.imshow(inference_mask[:,:,1], alpha=1)
+#     ax1.axis('off')
+#     
+#     
+
 def visualize_inference(inference_mask, input_img):
     
-    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,5))
-    ax1.imshow(io.imread(input_img))
-    ax2.imshow(inference_mask[:,:,1], alpha=1)
-    ax1.axis('off')
+    fig, ax= plt.subplots(1,1, figsize=(10,10))
+    ax.imshow(inference_mask[:,:,1], alpha=1)
+    ax.axis('off')
+    plt.savefig('static/mask.png', dpi = 500, pad_inches = 0)
+
+    fig2, ax2= plt.subplots(1,1, figsize=(10,10))
+    ax2.imshow(io.imread(input_img))
     ax2.axis('off')
-    plt.savefig('static/mask.png')
+    plt.savefig('static/img.png')
 
 
+    
 def make_prediction(input_image):
     learner = import_model()
     tens_img = prep_input(input_image)
-    mask = make_inference(learner, tens_img)
+    mask = inference_mask(learner, tens_img)
     visualize_inference(inference_mask=mask, input_img=input_image)
     
